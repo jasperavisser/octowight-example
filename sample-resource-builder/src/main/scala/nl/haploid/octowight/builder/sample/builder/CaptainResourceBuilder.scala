93e00e7322f58c582e806c3c125544b3bb3bb8da
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-07-30 00:08:09 +0200

    Simplify code a bit

--- sample-resource-builder/src/main/scala/nl/haploid/octowight/builder/sample/builder/CaptainResourceBuilder.scala
+++ sample-resource-builder/src/main/scala/nl/haploid/octowight/builder/sample/builder/CaptainResourceBuilder.scala
@@ -3,7 +3,7 @@ package nl.haploid.octowight.builder.sample.builder
 
 import nl.haploid.octowight.JsonMapper
 import nl.haploid.octowight.builder.builder.ResourceBuilder
-import nl.haploid.octowight.registry.data.{Atom, ResourceIdentifier, ResourceMessage, ResourceRoot}
+import nl.haploid.octowight.registry.data.{Atom, ResourceMessage, ResourceRoot}
 import nl.haploid.octowight.source.sample.repository.CaptainDmoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -17,20 +17,17 @@ class CaptainResourceBuilder extends ResourceBuilder {
 
   override val collection = "captain"
 
-  override def build(resourceRoots: Iterable[ResourceRoot]): Iterable[(ResourceMessage, Set[Atom])] = {
+  override def build(resourceRoots: Iterable[ResourceRoot]): Iterable[(ResourceMessage, Set[Atom])] =
     DB localTx { implicit session => buildWithSession(resourceRoots) }
-  }
 
   def buildWithSession(resourceRoots: Iterable[ResourceRoot])(implicit session: DBSession): Iterable[(ResourceMessage, Set[Atom])] = {
-    val personIds: Set[Long] = resourceRoots.map(_.root.id).toSet
-    val captainDmos = captainDmoRepository.findByPersonIds(personIds)
-    val captainDmosById = captainDmos.groupBy(_.personId)
+    val personIds = resourceRoots.map(_.root.id).toSet
+    val captainDmosById = captainDmoRepository.findByPersonIds(personIds)
+      .map(c => c.personId -> c).toMap
     resourceRoots.map { resourceRoot =>
-      val resourceIdentifier = new ResourceIdentifier(collection = resourceRoot.resourceCollection, id = resourceRoot.resourceId)
-      // TODO: there really shouldn't be multiple
+      val resourceIdentifier = resourceRoot.toResourceIdentifier
       captainDmosById.get(resourceRoot.root.id) match {
-        case Some(captainDmosForResourceRoot) =>
-          val captainDmo = captainDmosForResourceRoot.head
+        case Some(captainDmo) =>
           val model = captainModelFactory.build(captainDmo)
           val resource = new ResourceMessage(resourceIdentifier = resourceIdentifier, model = jsonMapper.serialize(model), tombstone = false)
           (resource, captainDmo.atoms(resourceRoot.root.origin))
@@ -40,6 +37,4 @@ class CaptainResourceBuilder extends ResourceBuilder {
       }
     }
   }
-
-
 }
